0c4f8377e6c90433a60ec758813216e710dce2f5
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-03-27 21:33:40 +0530

    27.03.2023 Assignment

---
+++ src/assignment1/Variables.java
@@ -0,0 +1,14 @@
+package assignment1;
+
+public class Variables {
+    public static void main(String[] args) {
+        String Name="Ajeet";
+                String Location="Kanpur";
+                String Class="BCA";
+        String Hobby=" Reding Books";
+        System.out.println("Student Details"+Name+Location+Class+Hobby);
+
+    }
+
+
+}
