56c91e6e0cdd6a1303e23035bb9ff4de3fc3f917
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-03-27 23:34:17 +0530

    27.03.2023 Swapping Assignment

---
+++ src/assignment1/Swapping.java
@@ -0,0 +1,14 @@
+package assignment1;
+
+public class Swapping {
+    public static void main(String[] args) {
+        System.out.println("Befor Sawapping");
+        int a=10;
+        int b=15;
+        System.out.println("Value of a:- "+a);
+        System.out.println("Value of b:- "+b);
+        System.out.println("After Sawapping");
+        System.out.println("Value of a:- "+b);
+        System.out.println("Value of b:- "+a);
+    }
+}
