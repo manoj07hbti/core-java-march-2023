e2e9fadd637347b2de8a7051f2ecde4dc58350cf
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-03-27 23:36:29 +0530

    27.03.2023 Swapping Assignment Changes

--- src/assignment1/Swapping.java
+++ src/assignment1/Swapping.java
@@ -2,12 +2,12 @@ package assignment1;
 
 public class Swapping {
     public static void main(String[] args) {
-        System.out.println("Befor Sawapping");
+        System.out.println("Before Swapping");
         int a=10;
         int b=15;
         System.out.println("Value of a:- "+a);
         System.out.println("Value of b:- "+b);
-        System.out.println("After Sawapping");
+        System.out.println("After Swapping");
         System.out.println("Value of a:- "+b);
         System.out.println("Value of b:- "+a);
     }
