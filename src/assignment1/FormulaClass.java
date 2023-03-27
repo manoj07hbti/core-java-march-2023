580a95e22594843779cf347bf59aff084204d2f8
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-03-27 22:20:56 +0530

    27.03.2023 formula Applied Programe

---
+++ src/assignment1/FormulaClass.java
@@ -0,0 +1,22 @@
+package assignment1;
+
+public class FormulaClass {
+    public static void main(String[] args) {
+        int a=10;
+        int b=12;
+        int sum=a+b;
+        System.out.println("Addtion :- "+sum);
+        int c=12;
+        int d=10;
+        int sub=c-d;
+        System.out.println("Subtract:- "+sub);
+        int e=12;
+        int f=8;
+        int multi=e*f;
+        System.out.println("Multiply:- "+multi);
+        int g=100;
+        int h=10;
+        int Div=g/h;
+        System.out.println("Divivde:- "+Div);
+    }
+}
