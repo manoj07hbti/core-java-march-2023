5502cd24000e2a80f4722b47238e179737d3f604
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-03-27 23:23:16 +0530

    27.03.2023 Variable Inserted Complete

--- src/assignment1/Variables.java
+++ src/assignment1/Variables.java
@@ -3,10 +3,32 @@ package assignment1;
 public class Variables {
     public static void main(String[] args) {
         String Name="Ajeet";
+        System.out.println("Student Name :-"+Name);
                 String Location="Kanpur";
+        System.out.println("City:- "+Location);
                 String Class="BCA";
+        System.out.println("Standard :-" + Class);
         String Hobby=" Reding Books";
-        System.out.println("Student Details"+Name+Location+Class+Hobby);
+        System.out.println("Hobby:- "+Hobby);
+        int Age=23;
+        System.out.println("Student Age:- "+Age);
+        double Marks=88.12;
+        System.out.println("Final Percentage:- "+Marks);
+        String Country="India";
+        System.out.println("Country Name"+Country);
+        String Nationalanimal="TTiger";
+        System.out.println("National Animal:- "+Nationalanimal);
+        String Captial="India";
+        System.out.println("India Capital:- "+Captial);
+        String Bestfriend ="Akash & Amit";
+        System.out.println("Best Fruends :- Akash  Amit"+Bestfriend);
+        String Project="Java";
+        System.out.println("Current Working Project:- "+Project);
+        int Assignment=3;
+        System.out.println("Java Class Asignment"+ Assignment);
+        int Timing= 3;
+        System.out.println("Assignment Timing:- "+Timing);
+
 
     }
 
