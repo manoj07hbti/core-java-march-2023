9c3b1a831533b35dad70af2149a9bf617fd07133
jhagaurav2020 <jha.gaurav2020@gmail,com>
2023-03-28 12:20:41 +0530

    20 variable and swapping without third variable use

---
+++ src/AssigmentVariable20.java
@@ -0,0 +1,157 @@
+public class AssigmentVariable20 {
+
+    public static void main(String[] args) {
+
+        int a=8;
+        int b=7;
+        int sum= a+b;
+        System.out.println("sum:-" +sum);
+
+        int c=798;
+        int u=757;
+        int abc= c+u;
+        System.out.println("abc:-"+abc);
+
+        int gaurav=986;
+        int amit=43535;
+        int abhsihek=6577;
+        int valuetriple= gaurav+amit+abhsihek;
+        System.out.println("valuetriple:-"+valuetriple);
+
+        int xcv=546;
+        int bvk=87;
+        int nvm=87;
+        int lesson= xcv+bvk+nvm;
+        System.out.println("lesson:-"+lesson);
+
+        int trip=687;
+        int weight=87;
+        int calculate= trip+weight;
+        System.out.println("calculate:-"+calculate);
+
+
+        int hunk=987;
+        int mayank=987;
+        int vowel= hunk*mayank;
+        System.out.println("vowel:-"+vowel);
+
+        int okk=5467;
+        int lkjh=9876;
+        int lopk=5464;
+        int look=6445;
+        int hair=okk*lkjh*lopk*look;
+        System.out.println("hair:-"+hair);
+
+
+        int m=567;
+        int n=543;
+        int month= m*n;
+        System.out.println("month:-"+month);
+
+        int main=12309;
+        int mom=546;
+        int day= main*mom;
+        System.out.println("day:-"+day);
+
+        int upp=4444;
+        int plk=675;
+        int like= upp*plk;
+        System.out.println("like:-"+like);
+
+
+        int fff=8766;
+        int oooo=87787;
+        int sub= fff-oooo;
+        System.out.println("sub:-"+sub);
+
+        int vive=98756;
+        int cock=1234;
+        int width=vive-cock;
+        System.out.println("width:-"+width);
+
+        int monk=45567;
+        int loin=5656;
+        int duke= monk-loin;
+        System.out.println("duke:-"+duke);
+
+
+        int hyper=7890;
+        int iopl=9876;
+        int boil= hyper-iopl;
+        System.out.println("boil:-"+boil);
+
+        int sunday=7;
+        int monday=1;
+        int substrication= sunday-monday;
+        System.out.println("substrication:-"+substrication);
+
+        int king=8098;
+        int fish=8999;
+        int dived=king/fish;
+        System.out.println("dived:-"+dived);
+
+        int college=5000;
+        int fess=4999;
+        int equal= college/fess;
+        System.out.println("equal:-"+equal);
+
+        int vvv=89487;
+        int nmnb=866559;
+        int asd= vvv/nmnb;
+        System.out.println("asd:-"+asd);
+
+        int ghjh=9876554;
+        int kijhj=9878676;
+        int doll=ghjh/kijhj;
+        System.out.println("doll:-"+doll);
+
+
+        int okla=44567878;
+        int dehli=44444;
+        int direction= okla/dehli;
+        System.out.println("direction:-"+direction);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
