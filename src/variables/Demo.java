f471bfc9d7026ea34cfe6700fd425009d0536dd4
Niranjan singh <ns201890@gmail.com>
2023-03-29 17:06:03 +0530

    29-march-2023-2

---
+++ src/variables/Demo.java
@@ -0,0 +1,23 @@
+package variables;
+
+public class Demo {
+    public static void main(String[] args) {
+     int a=40;
+     int b=20;
+     int sub=a-b;
+     System.out.println("sub "+sub);
+      int c=40;
+      int d=50;
+      int add=c+d;
+      System.out.println("add "+add);
+       double e=40;
+       double f=20;
+       double divide=e/f;
+       System.out.println("divide "+divide);
+       double g=500;
+       double h=20;
+       double percentage=g*h/100;
+       System.out.println("percentage "+percentage);
+
+    }
+}
