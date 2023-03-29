f1c85a07400cd68aa5b416454f344f3baa16d00d
Niranjan singh <ns201890@gmail.com>
2023-03-29 09:33:40 +0530

    29-march-2023

---
+++ src/variables/Dmm.java
@@ -0,0 +1,11 @@
+package variables;
+
+public class Dmm {
+    public static void main(String[] args) {
+        int age;
+                age=23;
+                double salary=2354.35344;
+                System.out.println("age "+age +"salary "+salary);
+        
+    }
+}
