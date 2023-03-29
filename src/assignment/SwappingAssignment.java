cda1119ebf2616bd86fb0986a98214ad887d3bca
vinay <sagavinay2001.sag@gmail.com>
2023-03-29 10:37:10 +0530

    swapping assignment

---
+++ src/assignment/SwappingAssignment.java
@@ -0,0 +1,17 @@
+package assignment;
+
+import com.sun.xml.internal.bind.v2.runtime.output.SAXOutput;
+
+public class SwappingAssignment {
+    public static void main(String args[]){
+        int a=50;
+        int b=200;
+        System.out.println("a ="+a +" b = "+b);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("After swapping");
+        System.out.println("a = "+a + " b = "+b);
+    }
+
+}
--- src/demo/DemoDay1.java
+++ src/demo/DemoDay1.java
@@ -2,9 +2,8 @@ package demo;
 
 public class DemoDay1 {
 
-    public static void main(String arga[]){
+    public static void main(String args[]){
 
         System.out.println("This is my second java program");
 
-    }
-}
+    }}
