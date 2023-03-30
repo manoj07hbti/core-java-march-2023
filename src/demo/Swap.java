4f805927b6584377a35c1b384e286404e711bb45
Niranjan singh <ns201890@gmail.com>
2023-03-30 16:16:06 +0530

    My swapping assignment

---
+++ src/demo/Swap.java
@@ -0,0 +1,16 @@
+package demo;
+public class Swap {
+    public static void main(String[] args) {
+     System.out.println("Before swapping");
+int a=10;
+int b=5;
+System.out.println("After swapping");
+System.out.println(" a: "+b);
+System.out.println("b: "+a);
+    }
+
+
+}
+
+
+
