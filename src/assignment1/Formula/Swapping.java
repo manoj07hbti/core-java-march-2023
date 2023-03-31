26040c33e7fb0016627c69b3f70fa38b7ad7205f
virendra-ravat <viruravat005@gmail.com>
2023-03-31 15:00:50 +0530

    Swap commit

---
+++ src/assignment1/Formula/Swapping.java
@@ -0,0 +1,23 @@
+package assignment1.Formula;
+
+public class Swapping {
+
+    public static void main(String[] args){
+        System.out.println("Before Swapping");
+        int a=10;
+        int b=15;
+
+        a =a+b;// its means value a=35
+
+        b = a-b;// its means value b=10
+        a =a-b;//its means value   a=15
+        System.out.println("Swap Variable a:-"+a);
+        System.out.println("Swap Variable b:-"+b);
+
+
+
+
+
+
+    }
+}
