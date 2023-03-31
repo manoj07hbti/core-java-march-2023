e090513e13342f29728b7ba3660327e968ffd318
virendra-ravat <viruravat005@gmail.com>
2023-03-31 11:37:52 +0530

    third commit

---
+++ src/demovariables/MyFirstClassVariable3.java
@@ -0,0 +1,30 @@
+package demovariables;
+
+public class MyFirstClassVariable3 {
+    public static void main(String[]args){
+
+        String name ="smile";
+        int age=24;
+        double salary=856483.544;
+        System.out.println("Printing name"+name+"printing age"+age+"salary"+salary);
+        int a=220;
+        int b=20;
+        int sum=a+b;
+        System.out.println("sum of a and b"+sum);
+
+        int c=10;
+        int d=5;
+        int sub=c-d;
+        System.out.println("sub of c and d"+sub);
+
+        int multi=c*d;
+        System.out.println("multi of c and d"+multi);
+
+        int divide= c/d;
+        System.out.println("div of c and d"+divide);
+
+
+
+
+    }
+}
