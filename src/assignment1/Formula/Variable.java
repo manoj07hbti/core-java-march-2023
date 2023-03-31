0430afd2055efd6aa3c6089becd1b2a7d529dae0
virendra-ravat <viruravat005@gmail.com>
2023-03-31 15:20:37 +0530

    variables commit

---
+++ src/assignment1/Formula/Variable.java
@@ -0,0 +1,24 @@
+package assignment1.Formula;
+
+import com.sun.xml.internal.ws.resources.BindingApiMessages;
+
+public class Variable {
+    public static void main(String[]args){
+
+        String name = "Viru Ravat";
+        System.out.println("Student name :-"+ name );
+        String Location = "Tundla";
+        System.out.println("City:-"+Location);
+        String Hobby = "Reading Books";
+        System.out.println("Hobby :-"+Hobby);
+        int Age=26;
+        System.out.println("Age:-"+ Age);
+        double Marks=86.6;
+        System.out.println("percent:-"+Marks);
+        String Country = "India";
+        System.out.println("Country Name:-"+Country);
+
+
+
+    }
+}
