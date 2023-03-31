f42d6c2a62e215b0c8f04949eebb593272184a1d
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-03-31 21:28:45 +0530

    Using And Opretor Assignment Complete

---
+++ src/conditional_statment_project/AndOpreatorProject.java
@@ -0,0 +1,7 @@
+package conditional_statment_project;
+
+public class AndOpreatorProject {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/conditional_statment_project/NestedIf.java
@@ -0,0 +1,7 @@
+package conditional_statment_project;
+
+public class NestedIf {
+    public static void main(String[] args) {
+
+    }
+}
