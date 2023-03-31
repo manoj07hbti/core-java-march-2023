17413ec2e8b1e6ce482e0cdbacaecf7e0d89aad7
virendra-ravat <viruravat005@gmail.com>
2023-03-31 10:00:48 +0530

    first commit

---
+++ src/demovariables/MyFirstClassVariables.java
@@ -0,0 +1,11 @@
+package demovariables;
+
+public class MyFirstClassVariables {
+    public static void main(String[] args) {
+        System.out.println("Hello this is my first class");
+        int a=40;
+        int b=10;
+        int add= a+b;
+        System.out.println("add of a and b "+add);
+    }
+}
