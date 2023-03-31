91a84ddd282594c7ca66be107e413d79e616e4bc
vinay <sagavinay2001.sag@gmail.com>
2023-03-31 21:52:46 +0530

    NESTED IF

---
+++ src/assignment/NestedIF.java
@@ -0,0 +1,22 @@
+package assignment;
+
+public class NestedIF {
+    public static void main(String[] args){
+        int marks=60;
+        if(marks>0){
+            if(marks>=33)
+                System.out.println("PASSED");
+        }else{
+            System.out.println("FAILED");
+        }
+
+        int price=5000;
+        if(price>1000){
+            if(price>=5000){
+                System.out.println("COSTLY");
+            }else{
+                System.out.println("AVERAGE");
+            }
+        }
+    }
+}
