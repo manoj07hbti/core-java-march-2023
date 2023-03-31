b8a4518025be904a4b9e09ca8d174a10a3e45932
Niranjan singh <ns201890@gmail.com>
2023-03-31 09:32:46 +0530

    if condition

---
+++ src/my_conditional_statement/FirstIfCondition.java
@@ -0,0 +1,39 @@
+package my_conditional_statement;
+
+public class FirstIfCondition {
+    public static void main(String[] args) {
+
+      // SYNTAX: if(expression){code}
+        // to check division of the student
+        int marks;// variable declaration
+         marks=70;
+         if(marks>=60){
+             System.out.println("Student gets first division");
+             marks=33;
+             if(marks<60){
+                 System.out.println("Student not cleared exam ");
+
+                // to check price
+                 int price;// variable declaration
+                 price=6000;
+                 if(price>=5000){
+                     System.out.println("it is costly product ");
+
+
+                            }
+
+
+
+
+
+
+
+
+             }
+        }
+
+
+
+
+    }
+}
