c139ece976417d620f19f96d3becf3d1f0f8b49e
virendra-ravat <viruravat005@gmail.com>
2023-03-31 12:04:37 +0530

    Formula commit

---
+++ src/assignment1/Formula/Java.java
@@ -0,0 +1,26 @@
+package assignment1.Formula;
+
+public class Java {
+    public static void main(String[]args){
+        int a=8;
+        int b=4;
+        int sum=a+b;
+        System.out.println("Addtion :- "+ sum);
+
+        int c=15;
+        int d=3;
+        int sub=c-d;
+        System.out.println("Subtraction :-"+sub);
+
+        int e=14;
+        int f=2;
+        int multi=e*f;
+        System.out.println("multiply :-"+multi);
+
+        int g=80;
+        int h=20;
+        int divide=g/h;
+        System.out.println("Divide :-"+divide);
+
+    }
+}
