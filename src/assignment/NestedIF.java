ebffb82cb523bb24d4c53e7f8396f8bee72642e5
vinay <sagavinay2001.sag@gmail.com>
2023-04-01 19:02:21 +0530

    NESTED IF

--- src/assignment/NestedIF.java
+++ src/assignment/NestedIF.java
@@ -4,11 +4,14 @@ public class NestedIF {
     public static void main(String[] args){
         int marks=60;
         if(marks>0){
-            if(marks>=33)
+            if(marks>=33){
                 System.out.println("PASSED");
         }else{
             System.out.println("FAILED");
         }
+        }else{
+        System.out.println("THE ENTERED MARKS IS NOT VALID");}
+
 
         int price=5000;
         if(price>1000){
