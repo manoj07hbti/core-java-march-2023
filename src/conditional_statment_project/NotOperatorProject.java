8ab75a4095fe55fd582e717cda4b19dd7fc485ea
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-04-05 00:50:57 +0530

    Using Not (!) Assignment Complete

--- src/conditional_statment_project/NotOperatorProject.java
+++ src/conditional_statment_project/NotOperatorProject.java
@@ -2,14 +2,49 @@ package conditional_statment_project;
 
 public class NotOperatorProject {
     public static void main(String[] args) {
+        String Grocery;
+        Grocery="Sugar";
 
+    //Not Operator use- Statement False--Covert True;
 
+        if(!(Grocery!="Sugar")){
+            System.out.println("Grocery Product is Sugar");
+        }else {
+            System.out.println("Grocery Product is not Sugar");
+        }
 
+        //Not Operator use- Statement True--Covert False;
+        int Marks;
+        Marks=45;
+        if(!(Marks>=40)){
+            System.out.println("Student Clear the Exam--");
+        }else {
+            System.out.println("Student did not clear the exam..");
+        }
 
+        //Using Both True & False Condition using And Operator;
+        //Using True Condition--Convert False
+        String Fruit;
+        int Amount;
+        Fruit="Apple";
+        Amount=80;
+        if(!(Fruit=="Apple" & Amount>=70)){
+            System.out.println("1 Kg Apple Cost is approx 80 rs..");
+        }else {
+            System.out.println("Apple not in reasonable price..");
+        }
 
+        //Using Both True & False Condition using And Operator;
+        //Using False Condition--Convert True
 
+        Fruit="Mango";
+        Amount=100;
+        if(!(Fruit=="Apple" & Amount<=70)){
+            System.out.println("1 Kg Mango Cost is approx 80 rs..");
+        }else {
+            System.out.println("Mango not in reasonable price..");
+        }
 
-
-
+        }
     }
-}
+
