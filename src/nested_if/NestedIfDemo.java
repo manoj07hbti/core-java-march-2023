dede7bfde2f6069068ac458dfa35d548ac0192d9
virendra-ravat <viruravat005@gmail.com>
2023-04-06 11:54:42 +0530

    nested if commit 2

--- src/nested_if/NestedIfDemo.java
+++ src/nested_if/NestedIfDemo.java
@@ -1,36 +1,46 @@
 package nested_if;
 
 public class NestedIfDemo {
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
         int age;
         age = 16; // Must be 18 years for drives license..
 
-           if (age>0);{
+        if (age > 0) ;
+        {
 
-              if (age>=18) {
-                  System.out.println("the license will be  made for a person"+age);
-              } else {
-                  System.out.println("the license will not be made for a person " + age);
-              }
-              String city= "Dehli";
+            if (age >= 18) {
+                System.out.println("the license will be  made for a person" + age);
+            } else {
+                System.out.println("the license will not be made for a person " + age);
+            }
+        }
+        age =5;
+        if(age>0){
 
-              if (city=="Agra"){
-
-                  if (age>=18) {
-
-                      System.out.println("Eligible for voting in Agra...");
-                  } else {
-                      System.out.println("Not Eligible for  voting");
-
-                  }
-                  }
+            if(age>=18) {
+                System.out.println("Adhar card will be   created " + age);
 
 
-              }
-              }
+
+            }else{
+                System.out.println("Adhar card will not be  created"+age);
+            }
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
