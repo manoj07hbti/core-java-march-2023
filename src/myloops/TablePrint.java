1a3752c871f28bc6312708a8826cf77862b74a4e
Niranjan singh <ns201890@gmail.com>
2023-04-07 15:05:21 +0530

    table_of_five

---
+++ src/myloops/TablePrint.java
@@ -0,0 +1,24 @@
+package myloops;
+
+public class TablePrint {
+    public static void main(String[] args) {
+
+        int number=5;
+
+        for (int i=1; i<=10; i++){
+
+            System.out.println("5 X "+i + " = "+number*i);
+
+        }
+        int j=1;
+        while(j<=10){
+
+            System.out.println(" While Loop 5 X "+j + " = "+number*j);
+            j++;
+        }
+
+    }
+}
+
+
+
