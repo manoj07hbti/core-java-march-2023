a655fc6f8ebfd483beba06a4ed9e51ee84c8d557
Prem Babu <premstudy123@gmail.com>
2023-04-08 22:52:55 +0530

    table 5 & 10 using do while loops

--- src/assignment_loops/EvenNumber.java
+++ src/assignment_loops/EvenNumber.java
@@ -1,23 +1,26 @@
 package assignment_loops;
 
 public class EvenNumber{
-    public static void main(String[]args){
+    public static void main(String[]args) {
         // print even numbers b/w 1 to 100 using for loop
 
-        for (int k=1;k<=100;k++){
+        for (int k = 1; k <= 100; k++) {
             // we check that number is is even
-            if (k%2==0){
-                System.out.println("for loop "+ k);
+            if (k % 2 == 0) {
+                System.out.println("for loop " + k);
             }
-//            // using while loop
-//            int i=1;
-//            while (i<=100){
-//                // we check number is even
-//                if (i%2==0){
-//                    System.out.println( "while loop"+i);
-//                }
-//                i++;
-//            }
+
         }
+
+        // using while loop
+        int i = 1;
+        while (i <= 100) {
+            // we check number is even
+            if (i % 2 == 0) {
+                System.out.println("while loop" + i);
+            }
+            i++;
+        }
+
     }
 }
---
+++ src/assignment_loops/Table_doWhileLoop.java
@@ -0,0 +1,24 @@
+package assignment_loops;
+
+public class Table_doWhileLoop {
+    public static void main(String[]args){
+        int i=1; // initial value
+        int num=5; // fixed value
+        do {
+            System.out.println("5 table do while loop 5x"+i+ " = " +i*num);
+            i++; // applied increment operator
+        }
+        while (i<=10); // here we close do while loop by ; symbol
+
+        // wrote code using do while loop for 10 table
+        i=1; // declare initial value
+        int num2=10; // fixed value
+        do {
+            System.out.println("10 table do while loop 10x"+i+ " = " +i*num2);
+            i++;
+        }
+        while (i<=10);// declare limits for applied loop
+        // here we close  do while loop by ; symbol
+    }
+
+}
