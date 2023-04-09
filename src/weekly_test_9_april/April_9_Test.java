3760e93fbc2547f60b84a13ba0fa073f5441223f
vinay <sagavinay2001.sag@gmail.com>
2023-04-09 20:55:49 +0530

    APRIL 9 TEST

---
+++ src/weekly_test_9_april/April_9_Test.java
@@ -0,0 +1,30 @@
+package weekly_test_9_april;
+
+public class April_9_Test {
+
+    public static void main(String[] args) {
+
+        //print even and odd number between 123 and 234
+
+        System.out.println("LIST OF EVEN AND ODD NUMBERS BETWEEN 123 AND 234 ");
+        for(int i=123;i<=234;i++){
+
+            //  % is used for remainder
+            if(i%2==0){
+                System.out.println("EVEN "+i );
+            }else{
+                System.out.println("ODD " +i);
+            }
+        }
+        for(int i=123;i<=234;i++){
+            if(i%5==0){
+                System.out.println(+i +" DIVISIBLE BY 5");
+        }}
+
+
+
+        }
+
+
+    }
+
