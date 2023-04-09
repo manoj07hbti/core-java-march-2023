e41dee59865d8aa987e32b29ac61472161c885c9
Prem Babu <premstudy123@gmail.com>
2023-04-09 18:56:31 +0530

    only even num b/w 1 to 100 using  do while loop

---
+++ src/assignment_loops/EvenNumDoWhileLoop.java
@@ -0,0 +1,13 @@
+package assignment_loops;
+
+public class EvenNumDoWhileLoop {
+    public static void main(String[]args){
+
+        // here used do while loop
+        int j=1;  //declare initial value
+        do {if (j%2==0) //check condition of even num
+            System.out.println("even num do while loop " +j);
+            j++;
+        }while (j<=100); // declare last value or limit
+    }
+}
