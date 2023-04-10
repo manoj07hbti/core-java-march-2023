fee553c40a420e09d3c7dc241fb9d1af515eddc2
Niranjan singh <ns201890@gmail.com>
2023-04-10 16:40:43 +0530

    Even_and_Odd_NUmber_do_while_loop

--- src/myloops/DoWhileLoopNumber.java
+++ src/myloops/DoWhileLoopNumber.java
@@ -3,20 +3,21 @@ package myloops;
 public class DoWhileLoopNumber {
     public static void main(String[] args) {
 
+int a=2;
+do{
+    System.out.println("all even number 1 to 100: "+a);
+    a+=2;
+}while(a<100);
+int b=1;
+System.out.println("Odd numbers");
+do{
+    System.out.println("all odd number 1 to 100: "+b);
+    b+=2;
+}while(b<100);
 
-        int number = 1;
-        do {
-            System.out.println("all even number "+number +"");
-            number++;
-        }while(number<=100);{
-            if(number%2==0){
-                System.out.println("even number "+number +"");
-            }else{
-                System.out.println("odd number "+number +"");
-            }
         }
     }
-}
+
 
 
 
