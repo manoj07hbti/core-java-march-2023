72a1af176358cd4b91561407feb1d8ae9a0868cd
virendra-ravat <viruravat005@gmail.com>
2023-04-11 23:53:41 +0530

    odd while loop

---
+++ src/assignment_loops/OddNumberByWhileLoop.java
@@ -0,0 +1,18 @@
+package assignment_loops;
+
+public class OddNumberByWhileLoop {
+    public static void main(String[]args){
+        int i=1;
+        //  declare last limit of number
+        while (i<=70){
+            // check odd number
+            if (!(i%2==0)) {
+                // print odd number
+                System.out.println("only for odd number  " + i);
+            }
+                 i++;
+            }
+        }
+
+    }
+
