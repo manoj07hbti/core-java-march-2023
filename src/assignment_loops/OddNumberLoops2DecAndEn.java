deaea2834786c3d8c450238a8e537eaeb953e59d
virendra-ravat <viruravat005@gmail.com>
2023-04-11 11:26:32 +0530

    for loop odd num commit

---
+++ src/assignment_loops/OddNumberLoops2DecAndEn.java
@@ -0,0 +1,21 @@
+package assignment_loops;
+
+public class OddNumberLoops2DecAndEn {
+    public static void main(String[]args){
+
+        //SYNTAX: for (start_condition;expression; increment){
+        //
+        //              }
+         // Increment for loop;
+
+        for(int i=1;i<=50;i++){
+            System.out.println("Hello Java"+i);
+
+        }
+        // Decrement for loop;
+        for(int i=45;i>0; i--){
+            System.out.println("Decrement for Java"+i);
+        }
+    }
+
+}
