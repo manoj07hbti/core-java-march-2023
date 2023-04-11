a0b40a84d66e139ed7cf6eb9a5c75d5005727f5f
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-04-12 02:48:24 +0530

    Array Project While & Do While Loop using Project Complete

---
+++ src/array_demo_project/ArrayProjectUsingwhile_Dowhile.java
@@ -0,0 +1,36 @@
+package array_demo_project;
+
+public class ArrayProjectUsingwhile_Dowhile {
+    public static void main(String[] args) {
+
+        // Using While loop for Printing Array Data:-==========================================
+        String Country[]={"India","Japan","USA","South Africa"};
+        int i=0;
+        while (i<Country.length){
+            System.out.println("Printing Country Name"+" = "+ Country[i]);
+            i++;
+        }
+
+        // Using Do While loop for Printing Array Data:-========================================
+        int Distance[]={15,14,19,22,36,45};
+        int j=0;
+        do {
+            System.out.println("Printing Distance"+" = "+Distance[j]+ " K.m");
+            j++;
+        }while (j<Distance.length);
+
+        // Using Advance While loop for Printing Array Data:-=====================================
+
+        while(){
+            System.out.println("Printing Country Name"+" = ");
+        }
+
+        // Using Advance  Do While loop for Printing Array Data:-=====================================
+
+        do {
+            System.out.println("Printing Distance"+ " = ");
+        }while ();
+
+        }
+    }
+
