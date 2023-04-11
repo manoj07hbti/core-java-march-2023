3995a12f24c47239b753209f5b2cc60ce1db6713
virendra-ravat <viruravat005@gmail.com>
2023-04-11 11:55:19 +0530

    odd number commit

--- src/assignment_loops/OddNumberLoops.java
+++ src/assignment_loops/OddNumberLoops.java
@@ -3,12 +3,12 @@ package assignment_loops;
 public class OddNumberLoops {
     public static void main(String[] args) {
         int number = 100;
-        System.out.println("The odd numbers are:");
+        System.out.println("The odd numbers are:" );
         for (int i = 1; i <= 100; i++) {
             if (i % 2 != 0) {
-            }
-            System.out.println(i+"");
+                System.out.println(i + "");
 
+            }
         }
     }
 }
