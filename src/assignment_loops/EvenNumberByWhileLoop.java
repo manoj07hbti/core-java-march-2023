43df099e165ec56b9621ae8f5adbe48bf8893ee2
virendra-ravat <viruravat005@gmail.com>
2023-04-11 23:41:36 +0530

    even while loop

---
+++ src/assignment_loops/EvenNumberByWhileLoop.java
@@ -0,0 +1,24 @@
+package assignment_loops;
+
+public class EvenNumberByWhileLoop {
+    public static void main(String[]args){
+ // even number 1 to 100 using while loop
+
+        int i=1;
+        // declare last number
+        while (i<=100){
+
+      // check even number
+      if (i%2==0) {
+          System.out.println("" + i);
+      }
+      // increment
+            i++;
+      }
+        }
+    }
+
+
+
+
+
