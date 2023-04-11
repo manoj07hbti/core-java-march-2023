762881a59dc6d84a540105f3bb39a89d2dde9c06
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-04-11 23:37:31 +0530

    Using Loop Back Project Complete

--- src/loops/EvenNumberForWhileUsingProject.java
+++ src/loops/EvenNumberForWhileUsingProject.java
@@ -7,9 +7,18 @@ public class EvenNumberForWhileUsingProject {
         int Number=100;
         for (int i = 1;i <=Number;i++){
             if (i % 2 == 0){
-                System.out.println("Even Number"+i);
+                System.out.println("Even Number"+ "="+i);
             }
+            }
+        // Using While loop & If Condition to Print Even Number;
+        int j=1;
+        while (j<Number){
+            if (j%2==0){
+                System.out.println("Even No"+"="+j);
+            }
+            j++;
             }
         }
-            }
+    }
+
 
--- src/loops/OddNumberForWhileUsingProject.java
+++ src/loops/OddNumberForWhileUsingProject.java
@@ -6,9 +6,16 @@ public class OddNumberForWhileUsingProject {
         int Number=100;
         for (int j = 1;j <=Number;j++){
             if (j % 2 != 0){
-                System.out.println("Odd Number"+j);
-
+                System.out.println("Odd Number"+"="+j);
             }
+        }
+        // Using While loop & If Condition to Print Even Number;
+        int k=1;
+        while (k<Number){
+            if (k%2!=0){
+                System.out.println("Odd Number"+"="+k);
+            }
+            k++;
         }
     }
 }
