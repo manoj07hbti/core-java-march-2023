45d1a9c840d59974313d76c046736f4486234476
Niranjan singh <ns201890@gmail.com>
2023-04-11 11:24:07 +0530

    array-with-for-loop

---
+++ src/myarray/DemoArray.java
@@ -0,0 +1,24 @@
+package myarray;
+
+import javax.sql.rowset.serial.SQLOutputImpl;
+
+public class DemoArray {
+    public static void main(String[] args) {
+String country1="India";
+String country2="usa";
+String country3="Africa";
+String country4="Australia";
+String country5="England";
+        //Datatype varName [] = {value1, value2,value3.......value N};
+        String countries []={"India","usa","Africa","Australia","England"};
+
+// printing array element based on index  varName[INDEX];
+
+        System.out.println("Print  array: "+countries[4]);
+// printing all element of array
+        for (int i=0; i<countries.length; i++){
+            System.out.println("printing all array:  "+countries[i]);
+        }
+
+    }
+}
