c7614c5573b2f9a228b94fcbbcdb6ab3898d72c3
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-04-14 01:28:12 +0530

    Break Loop Project Complete

---
+++ src/loops/Break_loop_Project.java
@@ -0,0 +1,28 @@
+package loops;
+
+public class Break_loop_Project {
+    public static void main(String[] args) {
+
+        //Using Break Loop for Printing Student Marks if dint Get 30 % Marks Break;
+        //Project 1
+
+        int marks[]={45,49,68,38,40,77,28,88,78,67};
+            for(int var:marks){
+                System.out.println("Student Get- "+var+ " Marks");
+                if (var<30){
+                    break;
+                }
+            }
+
+        //Using Break Loop for Printing Student Marks if dint Get 30 % Marks Break;
+        //Project 2
+
+            String stream[]={"Science","Arts","Commerce","Biology","Computers"};
+            for (String var:stream){
+                System.out.println("Student Stream:-"+ var);
+                if (var=="Biology"){
+                 break;
+                }
+            }
+            }
+        }
---
+++ src/loops/Continue_Loop_Project.java
@@ -0,0 +1,9 @@
+package loops;
+
+public class Continue_Loop_Project {
+    public static void main(String[] args) {
+
+        //Using Continue Statement for Printing Java Courses Project
+
+    }
+}
