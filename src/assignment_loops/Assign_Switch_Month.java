1c4fcba8812d9558b99850bc408ac2eff5fa0a00
Prem Babu <premstudy123@gmail.com>
2023-04-14 19:14:17 +0530

    Assignment Switch month

---
+++ src/assignment_loops/Assign_Switch_Month.java
@@ -0,0 +1,54 @@
+package assignment_loops;
+
+public class Assign_Switch_Month {
+    public static void main(String[]args){
+        int month=10;
+        switch (month){
+            case 1: // if (month= =1)
+                System.out.println("this month is January");
+                break;
+            case 2:
+                System.out.println("this month is Febuary");
+                break;
+            case 3:
+                System.out.println("this month is March");
+                break;
+            case 4:
+                System.out.println("this month is April");
+                break;
+            case 5:
+                System.out.println("this month is May");
+                break;
+            case 6:
+                System.out.println("this month is June");
+                break;
+            case 7:
+                System.out.println("this month is July");
+                break;
+            case 8:
+                System.out.println("this month is August");
+                break;
+            case 9:
+                System.out.println("this month is September");
+                break;
+            case 10:
+                System.out.println("this month is October");
+                break;
+            case 11:
+                System.out.println("this month is November");
+                break;
+            case 12:
+                System.out.println("this month is December");
+             break;
+
+            default:
+                System.out.println("Invalid month please enter correct number");
+
+
+
+
+
+
+        }
+    }
+}
