7501deb60a7c35e426e761a96226f037c941fe26
virendra-ravat <viruravat005@gmail.com>
2023-04-15 18:04:50 +0530

    Switch loops commit

---
+++ src/assignment_loops/Switch_loops.java
@@ -0,0 +1,50 @@
+package assignment_loops;
+
+public class Switch_loops {
+    public static void main(String[] args) {
+        int month=2;
+
+        switch (month){
+
+            case 1: //if  ( month = 1)
+                System.out.println("this month is January");
+                break;
+            case 2:
+                System.out.println("this month is February");
+                break;
+            case 3:
+                System.out.println("this month is march");
+                break;
+
+            case 4:
+                System.out.println("this month is April");
+                break;
+            case 5:
+                System.out.println("this month is May");
+                break;
+            case 6:
+                System.out.println("this month is June");
+                break;
+            case 7:
+                System.out.println("this month is July");
+                break;
+            case 8:
+                System.out.println("this month is August");
+                break;
+            case 9:
+                System.out.println("this month is September");
+                break;
+            case 10:
+                System.out.println("this month is October");
+                break;
+            case 11:
+                System.out.println("this month is November");
+                break;
+            case 12:
+                System.out.println("this month is December");
+                break;
+            default:
+                System.out.println("Invalid month please enter correct number");
+        }
+    }
+}
