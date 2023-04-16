5070eaf23d2e81d7f8e4a46cdb1a462d6c073213
jhagaurav2020 <jha.gaurav2020@gmail,com>
2023-04-16 20:30:25 +0530

    test_16_april_2023

---
+++ src/Test_16_april_2023/Factorial_given_number.java
@@ -0,0 +1,15 @@
+package Test_16_april_2023;
+
+public class Factorial_given_number {
+    public static void main(String[] args) {
+        int k,fact=1;
+        int number=9;
+
+        for (k=1;k<number;k++){
+            fact=fact*9;
+        }
+        System.out.println("factorial of number"  +number+ " is "+fact);
+
+
+    }
+}
---
+++ src/Test_16_april_2023/prime_and_or_not_number.java
@@ -0,0 +1,19 @@
+package Test_16_april_2023;
+
+public class prime_and_or_not_number {
+    public static void main(String[] args) {
+        int value =18;
+        boolean weather= false;
+        for (int h=2; h<=value;++h){
+            if (value % h ==0){
+                weather =true;
+                break;
+            }
+        }
+        if (!weather)
+            System.out.println(value+ "prime number");
+        else
+            System.out.println(value+ "is not prime number");
+
+        }
+       }
