99c667e996e7a3e42b832869470f2bda67456ff7
Niranjan singh <ns201890@gmail.com>
2023-04-16 21:04:12 +0530

    Age_test

---
+++ src/myloops/AgeTest.java
@@ -0,0 +1,18 @@
+package myloops;
+
+public class AgeTest {
+    public static void main(String[] args) {
+
+        int age;
+        String city;
+
+        age = 18;
+        city = "up";
+        if(age>=18 & city=="up"){
+            System.out.println("Person is eligible for voting");
+        }else{
+            System.out.println("Person is not eligible for voting");
+        }
+
+    }
+}
