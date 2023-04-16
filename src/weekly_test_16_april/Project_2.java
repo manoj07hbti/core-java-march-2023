9b37fb85907301adc306d885042b03c2bbe1262b
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-04-16 21:02:57 +0530

    All Test Assignment Complete of Test

--- src/weekly_test_16_april/Project_2.java
+++ src/weekly_test_16_april/Project_2.java
@@ -1,5 +1,16 @@
 package weekly_test_16_april;
 
 public class Project_2 {
-
+    //Write a program to calculate percentage of any number and
+    // it will also identify whether calculated % is between 10 to 30
+    public static void main(String[] args) {
+        int marks;
+        marks=24;
+        if(marks<30){
+            System.out.println("calculated % is between 10 to 30"+" Scored Marks:- "+ marks);
+        }else {
+            System.out.println("calculated % is not between 10 to 30");
+        }
+    }
 }
+
--- src/weekly_test_9_april/Programe2.java
+++ src/weekly_test_9_april/Programe2.java
@@ -5,12 +5,10 @@ public class Programe2 {
     //Write a program to calculate percentage of any number and
     // it will also identify whether calculated % is between 10 to 30
     public static void main(String[] args) {
-       double Total=472.0;
-       double Marks=6;
-       double Percentage=Total/Marks;
-        System.out.println("Percentage "+"="+Percentage);
-        if(Percentage>30){
-            System.out.println("calculated % is not between 10 to 30");
+        int marks;
+        marks=29;
+        if(marks<30){
+            System.out.println("calculated % is between 10 to 30"+" Scored Marks:- "+ marks);
         }else {
             System.out.println("calculated % is not between 10 to 30");
         }
