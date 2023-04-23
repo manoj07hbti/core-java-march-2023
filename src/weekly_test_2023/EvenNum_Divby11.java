c67c80c36a5439d96f4a3a30ceced6ecd359607d
Prem Babu <premstudy123@gmail.com>
2023-04-23 20:22:11 +0530

    Check b/w 180 to 1 num is divb y11 and shoud be Even num

---
+++ src/weekly_test_2023/EvenNum_Divby11.java
@@ -0,0 +1,27 @@
+package weekly_test_2023;
+
+public class EvenNum_Divby11 {
+    public static void main(String[] args) {
+
+        // using for loop and applying Nested if condition
+        int i = 180;
+        for (i = 180; i >= 1; i--) {
+            if (i % 11 == 0) {
+                if (i % 2 == 0) {
+                    System.out.println("Number is div by 11 and this is even num" + i);
+                }
+            }
+        }
+
+        // we using while loop as well & operator
+        i=180; // initial value
+        while (i >= 1) {
+            if (i % 11 == 0 & i % 2 == 0) {
+                System.out.println("Number is div by 11 and this is even num using while loop & operator" + i);
+            }
+            i--; // decrement operator
+
+
+        }
+    }
+}
