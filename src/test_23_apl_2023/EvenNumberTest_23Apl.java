a0b162647dad9efd276f82c5b980cdf1458c2b6b
virendra-ravat <viruravat005@gmail.com>
2023-04-23 21:29:10 +0530

    test 23Apl commit2

--- src/test_23_apl_2023/EvenNumberTest_23Apl.java
+++ src/test_23_apl_2023/EvenNumberTest_23Apl.java
@@ -1,6 +1,6 @@
 package test_23_apl_2023;
 
-public class DemoTest_23Apl {
+public class EvenNumberTest_23Apl {
     public static void main(String[] args) {
         int number = 190;
         System.out.println("list of even number from 7 to " + number);
