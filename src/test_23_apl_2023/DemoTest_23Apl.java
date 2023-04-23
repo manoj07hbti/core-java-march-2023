c58e8a4d6962a13d8addeb1559189f10ca8b035e
virendra-ravat <viruravat005@gmail.com>
2023-04-23 20:43:28 +0530

    test 23Apl commit

---
+++ src/test_23_apl_2023/DemoTest_23Apl.java
@@ -0,0 +1,17 @@
+package test_23_apl_2023;
+
+public class DemoTest_23Apl {
+    public static void main(String[] args) {
+        int number = 190;
+        System.out.println("list of even number from 7 to " + number);
+
+        for (int i = 7; i <= number; i++) {
+
+            if (i % 2 == 0 && i % 7 == 0) {
+
+                System.out.println(i+"");
+            }
+        }
+
+    }
+}
