d2b68e73665335b8e0e0b485acfa296df5e7cbf2
virendra-ravat <viruravat005@gmail.com>
2023-04-29 21:32:57 +0530

    IF CONDITIONAL COMMIT

---
+++ src/conditional_statment_practice_2/Use_Of_If_conditional_1.java
@@ -0,0 +1,51 @@
+package conditional_statment_practice_2;
+
+public class Use_Of_If_conditional_1 {
+
+    public static void main(String[] args) {
+        int age;//Variable declaration
+         age = 25;
+        if (age >= 25);
+        System.out.println("The person can do the court marriage "+age);
+
+        // to check person is old
+        age = 65;
+        if (age >=65);
+        System.out.println("Person is old age "+age);
+
+        // to check person is eligible for voting ;
+        age = 18;
+        if (age >=18);
+        System.out.println("person is eligible for voting "+age);
+
+        // to check person is available for driving license;
+        age = 18 ;
+        if (age >=18);
+        System.out.println("person is  available for driving license " +age);
+
+        //   //     //    ///    ///    ///     //     ///      //   ///       ///     //    ///      ///
+
+        age = 17;
+        if(age >= 25);
+        System.out.println("the person can not the court marriage "+age);
+
+        ///       ///     ///     ///      ///         ////         ///     ///     ///        ///       ///
+
+        age = 27;
+        if (age >= 60);
+        System.out.println("the person is not old age "+age);
+
+       ///     ///     ///     ///      ///         ////         ///     ///     ///        ///       ///
+
+        age = 16;
+        if (age >= 18);
+        System.out.println("the person is not eligible for voting "+ age);
+
+        ///     ///     ///     ///      ///         ////         ///     ///     ///        ///       ///
+
+        age = 15;
+        if (age >= 18);
+        System.out.println("the person is not available  for driving license  "+ age);
+
+    }
+}
