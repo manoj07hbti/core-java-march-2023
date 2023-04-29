af0c778698cfb3dba51ec01eeb4a368d4f6b0d9e
virendra-ravat <viruravat005@gmail.com>
2023-04-29 16:15:21 +0530

    Method of the cube commit

---
+++ src/method_or_function/Method_Of_Cube.java
@@ -0,0 +1,23 @@
+package method_or_function;
+
+public class Method_Of_Cube {
+
+    public void make_cube() {
+        int a= 7;
+        int cube = a * a * a;
+        System.out.println("cube given of the number " + cube);
+    }
+
+    public static void main(String[] args) {
+        Method_Of_Cube obj = new Method_Of_Cube();
+        obj. make_cube();
+        obj. make_cube();
+
+
+    }
+    }
+
+
+
+
+
