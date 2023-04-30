f8195cfa1afbc699bb3dfccd40f2cff68272e7b8
virendra-ravat <viruravat005@gmail.com>
2023-04-30 12:24:00 +0530

    IF And Else Condition commit 2

---
+++ src/conditional_statment_practice_2/If_And_Else.java
@@ -0,0 +1,20 @@
+package conditional_statment_practice_2;
+
+public class If_And_Else {
+    public static void main(String[] args) {
+
+        String city = "Dehli";
+        if (city=="Dehli"){
+           System.out.println("person belong to dehli");
+        }else {
+            System.out.println("person does not belong to dehli");
+        }
+            int age = 21;
+            if (age>=18){
+                System.out.println("person is eligible for voting");
+            }else {
+                System.out.println("person is not eligible for voting");
+            }
+        }
+    }
+
