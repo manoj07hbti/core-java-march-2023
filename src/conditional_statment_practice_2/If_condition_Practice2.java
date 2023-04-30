f8af237ff45387056b82cda2985296431b3797b9
virendra-ravat <viruravat005@gmail.com>
2023-04-30 05:56:36 +0530

    IF CONDITIONAL COMMIT 3

---
+++ src/conditional_statment_practice_2/If_condition_Practice2.java
@@ -0,0 +1,18 @@
+package conditional_statment_practice_2;
+
+public class If_condition_Practice2 {
+    public static void main(String[] args) {
+        String city = "Agra";
+        if (city == "Agra");
+        System.out.println("Person belongs to Agra");
+
+        // To check person does not belong to Agra:
+
+        city ="Tundla";
+        if (city!="Agra");
+        System.out.println("the person does not belong to Agra >>> "+city);
+
+
+
+    }
+}
