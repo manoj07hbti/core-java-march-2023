4ab9168fe815d10a53444c579263dd774ac91b26
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-05-05 01:20:03 +0530

    Even Odd Project Output Not Print

--- src/methods_or_function/Output_Parameter_Project.java
+++ src/methods_or_function/Output_Parameter_Project.java
@@ -34,8 +34,6 @@ public class Output_Parameter_Project {
         }
     }
 
-
-
     public static void main (String[]args){
         Output_Parameter_Project obj = new Output_Parameter_Project();
         int cube = obj.makeCube(12);
@@ -50,8 +48,7 @@ public class Output_Parameter_Project {
 
             // 3rd Program Output========================================
             for (int i=1;i<=100;i++){
-                boolean output=obj.findNumber(i);
-                if(output){
+                if(obj.findNumber(i)){
                     System.out.println("Even Number :- "+i);
             }else {
                     System.out.println("Odd Number :- "+i);
