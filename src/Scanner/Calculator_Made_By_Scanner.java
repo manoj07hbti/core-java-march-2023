022f6dd8a07228707fd29583147e4e8740f98c0c
Manoj Kumar <manojkumar.pcf@gmail.com>
2023-05-08 20:04:52 +0530

    coreection

--- src/Scanner/Calculator_Made_By_Scanner.java
+++ src/Scanner/Calculator_Made_By_Scanner.java
@@ -3,23 +3,27 @@ package Scanner;
 import java.util.Scanner;
 
 public class Calculator_Made_By_Scanner {
-    public int addition(int a, int b){
-        int sum=a+b;
+    public int addition(int a, int b) {
+        int sum = a + b;
         return sum;
     }
-    public int sub(int a, int b){
-        int sub=a-b;
+
+    public int sub(int a, int b) {
+        int sub = a - b;
         return sub;
     }
-    public  int multiply(int a, int b){
-        int multi = a*b;
+
+    public int multiply(int a, int b) {
+        int multi = a * b;
         return multi;
     }
-    public int divide (int a, int b) {
+
+    public int divide(int a, int b) {
         int div = a / b;
         return div;
     }
-    public int percentage(int a , int  b) {
+
+    public int percentage(int a, int b) {
         int per = a * b / 100;
         return per;
     }
@@ -27,14 +31,26 @@ public class Calculator_Made_By_Scanner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter value of a: ");
-        int a= scanner.nextInt();
+        int a = scanner.nextInt();
         System.out.println("Please enter value of b: ");
-        int b= scanner.nextInt();
-Calculator_Made_By_Scanner obj = new Calculator_Made_By_Scanner();
-        System.out.println("Addi is "+obj.addition(a,b));
-        System.out.println("sub is "+obj.sub(a,b));
-        System.out.println("multi is "+obj.multiply(a,b));
-        System.out.println("divide is "+obj.divide(a,b));
-        System.out.println("per is "+obj.percentage(a,b));
+        int b = scanner.nextInt();
+        System.out.println("Please enter your choice : 1- Add, 2-Sub, 3- multi, 4-divide and 5 for percentage");
+        int choice = scanner.nextInt();
+
+        Calculator_Made_By_Scanner obj = new Calculator_Made_By_Scanner();
+
+        if (choice == 1) {
+            System.out.println("Addi is " + obj.addition(a, b));
+        } else if (choice == 2) {
+            System.out.println("sub is " + obj.sub(a, b));
+        } else if (choice == 3) {
+            System.out.println("multi is " + obj.multiply(a, b));
+        } else if (choice == 4) {
+            System.out.println("divide is " + obj.divide(a, b));
+        } else {
+            System.out.println("per is " + obj.percentage(a, b));
+        }
+
+
     }
 }
