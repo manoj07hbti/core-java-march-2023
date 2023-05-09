2f2e30ea583b9986ece6174659856ca477eadf16
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-05-10 02:29:19 +0530

    Calculator App (Some issue)

--- src/methods_or_function/Scanner_Class_Choice_Project.java
+++ src/methods_or_function/Scanner_Class_Choice_Project.java
@@ -2,7 +2,7 @@ package methods_or_function;
 
 import java.util.Scanner;
 
-public class Scanner_Class_Project {
+public class Scanner_Class_Choice_Project {
     public int addition(int a, int b) {
         int sum = a + b;
         return sum;
@@ -23,17 +23,25 @@ public class Scanner_Class_Project {
         return div;
     }
 
+
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         System.out.println("Please enter the value of :- a");
         int a= scanner.nextInt();
         System.out.println("Please enter the value of :- b");
         int b= scanner.nextInt();
+        System.out.println("Enter your choice:- ");
+        int choice=scanner.nextInt();
 
-        Scanner_Class_Project obj=new Scanner_Class_Project();
-        System.out.println("Addition is :-"+obj.addition(a,b));
-        System.out.println("Subtract is :- "+obj.subtract(a,b));
-        System.out.println("Multiply is :- "+obj.multiply(a,b));
-        System.out.println("Divide is :- "+ obj.divide(a,b));
+        Scanner_Class_Choice_Project obj=new Scanner_Class_Choice_Project();
+        if (choice==1){
+            System.out.println("Addition is :-"+obj.addition(a,b));
+        } else if (choice==2) {
+            System.out.println("Subtract is :- "+obj.subtract(a,b));
+        } else if (choice==3) {
+            System.out.println("Multiply is :- "+obj.multiply(a,b));
+        } else if (choice==4) {
+            System.out.println("Divide is :- "+ obj.divide(a,b));
+        }
     }
 }
