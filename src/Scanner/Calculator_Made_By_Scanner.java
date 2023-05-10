be93d29c90b88f97378f585947ffb236e002a2b9
virendra-ravat <viruravat005@gmail.com>
2023-05-10 10:07:52 +0530

    Scanner commit 1

--- src/Scanner/Calculator_Made_By_Scanner.java
+++ src/Scanner/Calculator_Made_By_Scanner.java
@@ -3,38 +3,62 @@ package Scanner;
 import java.util.Scanner;
 
 public class Calculator_Made_By_Scanner {
-    public int addition(int a, int b){
-        int sum=a+b;
+    public int addition(int a, int b) {
+        int sum = a + b;
         return sum;
     }
-    public int sub(int a, int b){
-        int sub=a-b;
+
+    public int sub(int a, int b) {
+        int sub = a - b;
         return sub;
     }
-    public  int multiply(int a, int b){
-        int multi = a*b;
+
+    public int multiply(int a, int b) {
+        int multi = a * b;
         return multi;
     }
-    public int divide (int a, int b) {
+
+    public int divide(int a, int b) {
         int div = a / b;
         return div;
     }
-    public int percentage(int a , int  b) {
+
+    public int percentage(int a, int b) {
         int per = a * b / 100;
         return per;
     }
 
     public static void main(String[] args) {
+        Calculator_Made_By_Scanner obj = new Calculator_Made_By_Scanner();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter value of a: ");
-        int a= scanner.nextInt();
-        System.out.println("Please enter value of b: ");
-        int b= scanner.nextInt();
-Calculator_Made_By_Scanner obj = new Calculator_Made_By_Scanner();
-        System.out.println("Addi is "+obj.addition(a,b));
-        System.out.println("sub is "+obj.sub(a,b));
-        System.out.println("multi is "+obj.multiply(a,b));
-        System.out.println("divide is "+obj.divide(a,b));
-        System.out.println("per is "+obj.percentage(a,b));
+        boolean flag = true;
+        while (flag) {
+
+            System.out.println("Please enter value of a: ");
+            int a = scanner.nextInt();
+            System.out.println("Please enter value of b: ");
+            int b = scanner.nextInt();
+
+            System.out.println("Please enter choice : + , - , * and / ");
+            Scanner scanner1 = new Scanner(System.in);
+            String choice = scanner1.nextLine();
+
+            if (choice.equals("+")) {
+                System.out.println("Addition is :" + obj.addition(a, b));
+            } else if (choice.equals("-")) {
+                System.out.println("Subtraction  is :" + obj.sub(a, b));
+            } else if (choice.equals("*")) {
+                System.out.println("multiply is :" + obj.multiply(a, b));
+            } else if (choice.equals("/")) {
+                System.out.println("divide is :" + obj.divide(a, b));
+            } else if (choice.equals("%")) {
+                System.out.println("percentage is :" + obj.percentage(a, b));
+                System.out.println("Do you want to close Calculator App? press Y to exit or press any other key to continue");
+                String new_choice = scanner1.nextLine();
+                if (new_choice.equals("Y")) {
+                    flag = false;
+                }
+            }
+        }
     }
 }
---
+++ src/Scanner/Calculator_Made_By_Scanner2.java
@@ -0,0 +1,29 @@
+package Scanner;
+
+import java.util.Scanner;
+
+public class Calculator_Made_By_Scanner2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System .in);
+
+        System.out.println("Enter two numbers");
+        int a = sc.nextInt();
+        int b =sc.nextInt();
+        System.out.println("Enter an operator do you want to calculator (+,-,*,/,%)");
+        char op = sc.next().charAt(0);
+        switch (op){
+
+            case '+': System.out.println(a+b);
+            break;
+            case '-': System.out.println(a-b);
+                break;
+            case '*': System.out.println(a*b);
+                break;
+            case '/': System.out.println(a/b);
+                break;
+            case '%': System.out.println(a%b);
+                break;
+            default:System.out.println("invalid input");
+        }
+    }
+}
