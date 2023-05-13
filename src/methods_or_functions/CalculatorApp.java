f0214ffd8b72e865d36e9e090061932343e46f78
Manoj Kumar <manojkumar.pcf@gmail.com>
2023-05-13 20:49:39 +0530

    Calc App

--- src/methods_or_functions/CalculatorApp.java
+++ src/methods_or_functions/CalculatorApp.java
@@ -17,6 +17,13 @@ public class CalculatorApp {
 
         return sub;
     }
+
+    public int multi(int a, int b){
+
+        int multi=a*b;
+
+        return multi;
+    }
     public double divide(int a, int b){
 
         double output=(double) a/b;
@@ -28,32 +35,39 @@ public class CalculatorApp {
         CalculatorApp obj= new CalculatorApp();
         Scanner scanner= new Scanner(System.in);
         boolean flag=true;
-
+        int output_1=0;
+        double output_2=1;
       while(flag){
           System.out.println("Please enter value of a: ");
           int a =scanner.nextInt();
           System.out.println("Please enter value of b: ");
           int b= scanner.nextInt();
-
           System.out.println("Please enter choice : + , - , * and / ");
           Scanner scanner1= new Scanner(System.in);
           String choice=scanner1.nextLine();
 
           if(choice.equals("+")){
-              System.out.println("Addition is :"+ obj.addition(a,b));
+              output_1+=obj.addition(a,b);
+              System.out.println("Addition is :"+output_1 );
           } else if (choice.equals("-")) {
-              System.out.println("Subtraction  is :"+ obj.sub(a,b));
+              output_1-=obj.sub(a,b);
+              System.out.println("Subtraction  is :"+ output_1);
           } else if (choice.equals("*")) {
-              //todo
+             output_2*=obj.multi(a,b);
+              System.out.println("Multiply  is :"+ output_2);
           } else {
-              System.out.println("Divide  is :"+ obj.divide(a,b));
+              output_2/=obj.divide(a,b);
+              System.out.println("Divide  is :"+ output_2);
           }
 
           System.out.println("Do you want to close Calculator App? press Y to exit or press any other key to continue");
          String new_choice=scanner1.nextLine();
          if(new_choice.equals("Y")){
+
              flag=false;
+
          }
+
       }
 
     }
