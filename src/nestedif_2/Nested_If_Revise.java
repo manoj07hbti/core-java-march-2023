ff062422f0e8114ccbe088cf984facd9f17d70a2
virendra-ravat <viruravat005@gmail.com>
2023-05-14 15:02:02 +0530

    NESTED IF COMMIT 2

---
+++ src/nestedif_2/Nested_If_Revise.java
@@ -0,0 +1,44 @@
+package nestedif_2;
+
+public class Nested_If_Revise {
+    public static void main(String[] args) {
+
+        int age;
+        age = 6;
+        if (age > 0) ;
+        {
+
+            if (age >= 5) {
+                System.out.println("The child is eligible for admission.");
+            } else {
+                System.out.println("The child is not eligible for admission");
+            }
+
+            String city;
+            city = "Tundla";
+
+            if (city == "Agra") {
+                if (age >= 5) {
+
+                    System.out.println("The child is eligible for admission Tundla");
+                } else {
+                    System.out.println("The child is not eligible for admission");
+                }
+            } else {
+                System.out.println("Only admission for Tundla students");
+
+
+                int marks;
+                marks = 33;
+                if (marks > 0) {
+                    if (marks >= 40) ;
+                    {
+                        System.out.println("Student cleared the exam");
+                    }
+                } else {
+                    System.out.println("Student not cleared the exam");
+                }
+            }
+        }
+    }
+}
