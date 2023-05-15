240fa2bab2961d6e317e78af6c6c527cb8940aef
vinay <sagavinay2001.sag@gmail.com>
2023-05-15 19:34:18 +0530

    ARMSTRONG NUMBER

--- src/assignment/Armstrong_Number.java
+++ src/assignment/Armstrong_Number.java
@@ -1,9 +1,14 @@
 package assignment;
 
+import java.util.Scanner;
+
 public class Armstrong_Number {
     public static void main(String[] args){
 
-        int number=153, arm=0,reminder, a;
+        int number, arm=0,reminder, a;
+        System.out.println("ENTER THE 3 DIGIT NUMBER");
+        Scanner scanner=new Scanner(System.in);
+        number=scanner.nextInt();
         a=number;
         while(number>0){
             reminder=number%10;
@@ -15,6 +20,5 @@ public class Armstrong_Number {
         }else{
             System.out.println("THE NUMBER "+a +" IS NOT AN ARMSTRONG NUMBER");
         }
-
     }
 }
