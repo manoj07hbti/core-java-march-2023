c6dc1d770358953bf1a181c78d282c4b8ed274b1
virendra-ravat <viruravat005@gmail.com>
2023-05-16 10:52:29 +0530

    AND OPERATOR REVISE DAY 2

--- src/con_statment_and_opretor_revise/And_Operator_Revise2.java
+++ src/con_statment_and_opretor_revise/And_Operator_Revise2.java
@@ -9,25 +9,90 @@ public class And_Operator_Revise2 {
 
         age = 18;
         city = "Pune";
-        if (age >= 18 & city == "Pune"){
+        if (age >= 18 & city == "Pune") {
 
             System.out.println("The person can play the cricket");
-        }else {
+        } else {
             System.out.println("The person not play the cricket");
         }
 
-         // TRUE & TRUE = TRUE;
+        // TRUE & TRUE = TRUE;
 
 
-        age = 5;
+        age = 6;
         city = "Agra";
-        if (age >=5 & city == "Agra"){
+        if (age >= 5 & city == "Agra") {
 
             System.out.println("The child is eligible for Adhar card");
-        }else {
+        } else {
             System.out.println("The child is not eligible for Adhar card");
         }
+
+        // TRUE & FALSE = FALSE;
+
+        age = 18;
+        city = "Pune";
+        if (age >= 18 & city == "Agra") {
+
+            System.out.println("The person can play the cricket");
+        } else {
+            System.out.println("The person not play the cricket");
+        }
+
+        // TRUE & FALSE = FALSE;
+
+        age = 6;
+        city = "Pune";
+        if (age >= 5 & city == "Agra") {
+
+            System.out.println("The child is eligible for Adhar card");
+        } else {
+            System.out.println("The child is not eligible for Adhar card");
+        }
+
+        // FALSE & TRUE = FALSE :
+
+        age = 17;
+        city = "Pune";
+        if (age >= 18 & city == "Pune") {
+
+            System.out.println("The person can play the cricket");
+        } else {
+            System.out.println("The person not play the cricket");
+        }
+
+        // TRUE & FALSE = FALSE;
+
+        age = 4;
+        city = "Pune";
+        if (age >= 5 & city == "Pune") {
+
+            System.out.println("The child is eligible for Adhar card");
+        } else {
+            System.out.println("The child is not eligible for Adhar card");
+        }
+
+        // FALSE & FALSE = FALSE;
+
+        age = 17;
+        city = "Agra";
+        if (age >= 18 & city == "Pune") {
+
+            System.out.println("The person can play the cricket");
+        } else {
+            System.out.println("The person not play the cricket");
+        }
+
+        // TRUE & FALSE = FALSE;
+
+        age = 4;
+        city = "Agra";
+        if (age >= 5 & city == "Pune") {
+
+            System.out.println("The child is eligible for Adhar card");
+        } else {
+            System.out.println("The child is not eligible for Adhar card");
         }
     }
-    
+}
 
