93faad4e20e77016e91b8a2b56d27c2a7be3f6c1
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-05-19 09:44:01 +0530

    Hashset Normal Project Complete

--- src/collection/Hashset/Hashset_Project.java
+++ src/collection/Hashset/Hashset_Project.java
@@ -1,4 +1,50 @@
 package collection.Hashset;
 
+import java.util.HashSet;
+
 public class Hashset_Project {
+    // Hash set use for auto filter duplicate data from our code==============================
+    // Example- We are Create Duplicate Data But Hash Set auto filter Duplicate Data==========================
+
+    //Project:- create hashset of String , INteger and Double and print Advanced loop
+    public void stringData(){
+        HashSet<String>studentName=new HashSet<>();
+        studentName.add("Rohit");
+        studentName.add("Ritesh");
+        studentName.add("Rohit");
+        studentName.add("Ritesh");
+
+        for (String var:studentName){
+            System.out.println("Name:- "+var);
+        }
+    }
+
+    public void integerData(){
+       HashSet<Integer>rollNumber=new HashSet<>();
+       rollNumber.add(12);
+       rollNumber.add(15);
+        rollNumber.add(12);
+        rollNumber.add(15);
+       for (Integer var:rollNumber){
+           System.out.println("Roll No:- "+var);
+       }
+    }
+
+    public void doubleData(){
+        HashSet<Double>percentage=new HashSet<>();
+        percentage.add(78.23);
+        percentage.add(88.32);
+        percentage.add(78.23);
+        percentage.add(88.32);
+        for (Double var:percentage){
+            System.out.println("Percentage:- "+var);
+        }
+    }
+
+    public static void main(String[] args) {
+        Hashset_Project obj=new Hashset_Project();
+        obj.stringData();
+        obj.integerData();
+        obj.doubleData();
+    }
 }
