def0fb0bf6bcf6d3669da3f93747686c6f847d56
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-05-19 09:56:45 +0530

    Complete Employee List with Hashset (Duplicate Not Done)

--- src/collection/Hashset/Employee_Hashset.java
+++ src/collection/Hashset/Employee_Hashset.java
@@ -1,5 +1,28 @@
 package collection.Hashset;
 
-public class Employee_Hashset {
+import collection.Model.Employee;
 
+import java.util.HashSet;
+
+public class Employee_Hashset {
+ public void employeeSet(){
+     HashSet<Employee>employees=new HashSet<>();
+     Employee employee1=new Employee("Rahul","Wipro","Software Engenier",18900.23);
+     Employee employee2=new Employee("Rohit","HCL","Backend Developer",25900.23);
+     Employee employee3=new Employee("Ajeet","Wipro","Frontend Developer",28900.23);
+
+     employees.add(employee1);
+     employees.add(employee2);
+     employees.add(employee3);
+
+     for (Employee var:employees){
+         System.out.println("Employee Name:- "+var.getName() + "Company Name:- "+ var.getCompany_name()+
+                 "Designation:- "+var.getDesignation()+ "Salary:- "+var.getSalary());
+     }
+ }
+
+    public static void main(String[] args) {
+     Employee_Hashset obj=new Employee_Hashset();
+     obj.employeeSet();
+    }
 }
