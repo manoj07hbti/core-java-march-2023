a9cc7acf97f6ec1622696700be8362dc8256dfff
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-05-24 09:50:05 +0530

    Complete Employee HashCode Create & Check Duplicate Data & Override Hashcode & Equals

--- src/collection/Hashset/Employee_Hashset.java
+++ src/collection/Hashset/Employee_Hashset.java
@@ -7,17 +7,26 @@ import java.util.HashSet;
 public class Employee_Hashset {
  public void employeeSet(){
      HashSet<Employee>employees=new HashSet<>();
-     Employee employee1=new Employee("Rahul","Wipro","Software Engenier",18900.23);
+     Employee employee1=new Employee("Rahul", "Wipro","Software Engenier",18900.23);
+     System.out.println("Hashcode Details Employee 1 :- "+employee1.hashCode());
      Employee employee2=new Employee("Rohit","HCL","Backend Developer",25900.23);
+     System.out.println("Hashcode Details Employee 2 :- "+employee2.hashCode());
      Employee employee3=new Employee("Ajeet","Wipro","Frontend Developer",28900.23);
+     System.out.println("Hashcode Details Employee 3 :- "+employee3.hashCode());
+
+
+     // Creating Duplicate to check Hashcode & Equals===========================
+     Employee employee4=new Employee("Ajeet","Wipro","Frontend Developer",28900.23);
+     System.out.println("Hashcode Details Employee 4 :- "+employee4.hashCode());
 
      employees.add(employee1);
      employees.add(employee2);
      employees.add(employee3);
+     employees.add(employee4);
 
      for (Employee var:employees){
-         System.out.println("Employee Name:- "+var.getName() + "Company Name:- "+ var.getCompany_name()+
-                 "Designation:- "+var.getDesignation()+ "Salary:- "+var.getSalary());
+         System.out.println("Employee Name:- "+ var.getName() + "Company Name:- "+ var.getCompany_name()+
+                 "Designation:- "+ var.getDesignation()+ "Salary:- "+ var.getSalary());
      }
  }
 
--- src/collection/Model/Employee.java
+++ src/collection/Model/Employee.java
@@ -1,11 +1,14 @@
 package collection.Model;
 
+import java.util.Objects;
+
 public class Employee {
     String name;
     String company_name;
     String designation;
     double salary;
 
+    //Create Param Constructor=================================
     public Employee(String name, String company_name, String designation, double salary) {
         this.name = name;
         this.company_name = company_name;
@@ -13,6 +16,24 @@ public class Employee {
         this.salary = salary;
     }
 
+    //Create Hashcode& Equals===================================
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Double.compare(employee.salary, salary) == 0 && Objects.equals(name, employee.name)
+                && Objects.equals(company_name, employee.company_name) && Objects.equals(designation, employee.designation);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, company_name, designation, salary);
+    }
+
+    // Create getter & setter=====================================
     public String getName() {
         return name;
     }
