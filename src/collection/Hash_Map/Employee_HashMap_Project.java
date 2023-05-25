b662e666dfbe2d836864a0c01b5014f893477499
Ajeetchauhan1999 <Ajeetchauhan363@gmail.com>
2023-05-26 01:15:03 +0530

    HashMap Project Complete

--- src/collection/Hash_Map/Employee_HashMap_Project.java
+++ src/collection/Hash_Map/Employee_HashMap_Project.java
@@ -5,11 +5,32 @@ import collection.Model.Employee;
 import java.util.HashMap;
 
 public class Employee_HashMap_Project {
-    public void createEmployeeHashMap(){
+    public HashMap<String, Employee> createEmployeeHashMap(){
         HashMap<String, Employee> employeeHashMap=new HashMap<>();
         //Creating Data=======================================
-        Employee employee1=new Employee("Rahul", "Wipro","Software Engenier",18900.23);
-        Employee employee2=new Employee("Rohit","HCL","Backend Developer",25900.23);
-        Employee employee4=new Employee("Ajeet","Wipro","Frontend Developer",28900.23);
+        Employee employee1=new Employee("Rahul", "IBM","Software Engenier",18900.23);
+        Employee employee2=new Employee("Rohit","CTS","Backend Developer",25900.23);
+        Employee employee3=new Employee("Ajeet","TCS","Frontend Developer",28900.23);
+        Employee employee4=new Employee("Akash","DXC","Frontend Developer",28900.23);
+
+        employeeHashMap.put("IBM",employee1);
+        employeeHashMap.put("CTS",employee2);
+        employeeHashMap.put("TCS",employee3);
+        employeeHashMap.put("DXC",employee4);
+
+        return employeeHashMap;
+    }
+
+    public static void main(String[] args) {
+        Employee_HashMap_Project obj=new Employee_HashMap_Project();
+        HashMap<String, Employee> employeeHashMap= obj.createEmployeeHashMap();
+
+        for (String var: employeeHashMap.keySet()){
+            if (var.equals("TCS")){
+                System.out.println("Printing Employee list:- ");
+                System.out.println(employeeHashMap.get(var).getName()+ "" +employeeHashMap.get(var).getCompany_name()
+                + "" +employeeHashMap.get(var).getDesignation()+ "" +employeeHashMap.get(var).getSalary());
+            }
+        }
     }
 }
