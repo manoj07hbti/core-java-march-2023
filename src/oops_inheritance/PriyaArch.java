24243ebebe2ddc268db3842ad6d9b1b1b62e873d
Prem Babu <premstudy123@gmail.com>
2023-05-31 19:32:02 +0530

    multi level Inheritance

---
+++ src/oops_inheritance/PriyaArch.java
@@ -0,0 +1,11 @@
+package oops_inheritance;
+// here implemented architecture class methods so we can say that mutilevel inher...
+public class PriyaArch extends MohanArch{
+    public static void main(String[] args) {
+        PriyaArch obj=new PriyaArch();
+        obj.readbook();
+        obj.createdesign();
+        obj.run();
+
+    }
+}
